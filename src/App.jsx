cc3a42740864d93fdc35563a8198f98bb9646837
greenFruits <mdabdullah6548ctg@gmail.com>
2025-10-03 14:40:57 +0600

    feat: added counter action's type and action creator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Counter from "./components/Counter";
+
 function App() {
-  return <></>;
+  return (
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
