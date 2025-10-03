a03fb2e2fb2f41d8e50a130293c915967d5b574b
greenFruits <mdabdullah6548ctg@gmail.com>
2025-10-03 21:53:19 +0600

    feat: successfully managed state using redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { decrement, increment } from "../redux/counter/actions";
 
-function Counter() {
+function Counter({ count, increment, decrement }) {
   return (
     <div class="max-w-md mx-auto mt-10 space-y-5">
       <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-        <div class="text-2xl font-semibold">0</div>
+        <div class="text-2xl font-semibold">{count}</div>
         <div class="flex space-x-3">
-          <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+          <button
+            onClick={increment}
+            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          >
             Increment
           </button>
-          <button class="bg-red-400 text-white px-3 py-2 rounded shadow">
+          <button
+            onClick={decrement}
+            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+          >
             Decrement
           </button>
         </div>
@@ -18,4 +26,16 @@ function Counter() {
   );
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    count: state.value,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment(5)),
+    decrement: () => dispatch(decrement(2)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
